8a2551b1f8ff8cd041632cf18c1a4e71ecd30110
Frikk Fossdal <frikkfossdal@gmail.com>
2019-02-18 22:43:34 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Plotting-Aparatus
+
+Project folder for general purpose plotter apparatus. 
+
+/doc/img/20190218-DSC_4306.jpg
---
+++ scripts/serial_parser/serial_parser.py
@@ -0,0 +1,35 @@
+import serial
+import time
+
+# open serial port
+s1 = serial.Serial('/dev/cu.usbmodem14201',250000)
+
+
+# Open g-code file
+f1 = open('demo2.gcode','r')
+
+# Wake up grbl
+s1.write(("\r\n\r\n").encode())
+time.sleep(2)   # Wait for grbl to initialize
+s1.flushInput()  # Flush startup text in serial input
+
+s1.write("G92 X0 Y0 Z0 \n".encode()) #go to machine home
+
+
+for line in f1:
+    l = line.strip()
+    print('sending: ' + l)
+    s1.write((l + '\n').encode())
+
+
+    grbl_out = s1.readline() # Wait for grbl response with carriage return
+    print(' : ' + grbl_out.strip().decode('utf-8'))
+    time.sleep(.1)
+
+# Wait here until grbl is finished to close serial port and file.
+
+input("  Press <Enter> to exit and disable grbl.")
+
+# Close file and serial port
+f.close()
+s1.close()
