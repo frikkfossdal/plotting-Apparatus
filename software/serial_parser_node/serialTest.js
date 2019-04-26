8b93297ec1535a6c819754a44f49f9fad4ba7bec
Frikk H Fossdal <frikkfossdal@gmail.com>
2019-04-26 16:35:59 +0200

    made a big mess. Have solution. Will clean on monday

---
+++ software/serial_parser_node/serialTest.js
@@ -0,0 +1,31 @@
+const SerialPort = require('serialport');
+const Readline = require('@serialport/parser-readline');
+const vocabulary = require('../gRouter/vocabulary.json');
+
+console.log(vocabulary[1].gcode);
+
+const port = new SerialPort('/dev/tty.usbmodem14201', {
+    baudRate: 250000
+})
+const parser = new Readline();
+port.pipe(parser)
+
+parser.on('data', function (data) {
+    console.log(data);
+    if (data == 'ok') {
+        let x = Math.random()*100; 
+        let y = Math.random()*200; 
+        port.write('G0 X' + x + ' Y'+y +'\n');
+    }
+});
+
+setInterval(()=>{port.write('G0 X0 Y0 \n');}, 1000); 
+
+
+
+//write logic 
+
+function sendCommand(message) {
+    port.write(String(message + '\n'))
+}
+//read logic
